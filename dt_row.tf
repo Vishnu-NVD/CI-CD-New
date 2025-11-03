ed4e16c868dfb51acf4c73623bc8d2f9b4b1ef37
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-11-03 15:57:02 +0530

    Update dt_row.tf

--- dt_row.tf
+++ dt_row.tf
@@ -1,5 +1,5 @@
 resource "genesyscloud_architect_datatable_row" "Vishnu M" {
-  datatable_id = genesyscloud_architect_datatable.agents.id
+  datatable_id = "3e997b05-7aa8-4949-b755-ac68604656a1"
   key_value    = "vishnu.muthusamy@servion.com"
   properties_json = jsonencode({
     "firstname" = "Vishnu"
