ec887adbde14c0ad2fec718a71f6a17e1f517c9e
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-11-10 16:00:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 1.70.0"
     }
   }
+ backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Cx_As_Code"         
+
+    workspaces {
+      name = "CI_CD"         
+    }
 }
 
 provider "genesyscloud" {}
