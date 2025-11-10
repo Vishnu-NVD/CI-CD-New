b6a263abf5370a84a25f7a940baaa8daff31a76a
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-11-10 12:37:34 +0530

    Update queue.tf

--- queue.tf
+++ queue.tf
@@ -1,1 +1,16 @@
-
+resource "genesyscloud_routing_queue" "my_queue" {
+  name                      = "Support Queue"
+  description               = "Queue for customer support"
+  media_settings_call {
+    alerting_timeout_seconds = 30
+  }
+  media_settings_chat {
+    alerting_timeout_seconds = 30
+  }
+  media_settings_email {
+    alerting_timeout_seconds = 30
+  }
+  media_settings_message {
+    alerting_timeout_seconds = 30
+  }
+}
