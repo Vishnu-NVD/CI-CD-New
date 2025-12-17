b739627fb26d068d97527b672f4630b31cfc8ff2
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-17 10:24:29 +0530

    Delete dt_row.tf

--- dt_row.tf
+++
@@ -1,9 +0,0 @@
-resource "genesyscloud_architect_datatable_row" "Vishnu_M" {
-  datatable_id = "3e997b05-7aa8-4949-b755-ac68604656a1"
-  key_value    = "vishnu.muthusamy@servion.com"
-  properties_json = jsonencode({
-    "firstname" = "Vishnu"
-    "lastname"    = "M"
-    "empid"        = 13748
-  })
-}
