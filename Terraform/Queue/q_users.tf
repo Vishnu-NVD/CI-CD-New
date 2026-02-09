e95e0d4b3f01d30a0964ac92f864aadec0a03058
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 16:45:51 +0530

    Delete Terraform/Queue/q_users.tf

--- Terraform/Queue/q_users.tf
+++
@@ -1,8 +0,0 @@
-resource "genesyscloud_routing_queue" "Queue_1" {
-  name = "Example Queue_CICD_demo_v2"
-
-members {
-    user_id  = "ef06fceb-e0f0-4558-84d2-7cb3acca9f2a"
-    ring_num = 2
-  }
-}
