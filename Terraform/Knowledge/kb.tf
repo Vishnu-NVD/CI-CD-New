da1859334dd634906bc382b93699da49a72e53f6
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 16:27:34 +0530

    Update and rename kb.tf to q.tf

--- Terraform/Knowledge/kb.tf
+++
@@ -1,8 +0,0 @@
-resource "genesyscloud_knowledge_knowledgebase" "example_knowledgebase" {
-  name          = "MyKB_via_CxAsCode_2"
-  description   = "An example knowledge base created via CI/CD"
-  core_language = "en-US"
-}
-
-
-
---
+++ Terraform/Queue/q.tf
@@ -0,0 +1,36 @@
+resource "genesyscloud_routing_queue" "Queue_1" {
+  name = "Example Queue_CICD_demo_v2"
+  media_settings_callback {
+    enable_auto_answer        = false
+    mode                      = "AgentFirst"
+    alerting_timeout_sec      = 30
+    auto_end_delay_seconds    = 300
+    service_level_duration_ms = 20000
+    service_level_percentage  = 0.8
+    auto_dial_delay_seconds   = 300
+  }
+  media_settings_chat {
+    enable_auto_answer        = false
+    service_level_duration_ms = 20000
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 30
+  }
+  media_settings_message {
+    service_level_duration_ms = 20000
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 30
+    enable_auto_answer        = false
+  }
+  media_settings_call {
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 8
+    enable_auto_answer        = false
+    service_level_duration_ms = 20000
+  }
+  media_settings_email {
+    service_level_duration_ms = 86400000
+    service_level_percentage  = 0.8
+    alerting_timeout_sec      = 300
+    enable_auto_answer        = false
+  }
+}
